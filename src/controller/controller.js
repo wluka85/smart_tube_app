29298041ae0b06e537f0a8964c2bf44202aa4587
wluka85 <wluka85@gmail.com>
2018-11-14 15:44:50 +0100

    Add authorization and sample of search result

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,4 +3,18 @@ export default class Controller {
     constructor(model) {
         this.model = model;
     }
+
+    searchVideo(accessToken) {
+            fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&q=elvis", {
+                method: 'GET',
+                headers: new Headers({'Authorization': 'Bearer ' + accessToken})
+
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data)
+                })
+
+
+    }
 }
--- src/view/view.js
+++ src/view/view.js
@@ -1,24 +1,36 @@
-import * as ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from "react-dom";
+import GoogleLogin from "react-google-login";
 
 export default class View {
     constructor(controller) {
         this.controller = controller;
         this.authorization = true;
+        this.addComponents();
     }
 
+    responseGoogle(response) {
+        this.controller.searchVideo(response.accessToken)
+    }
+
+
+
     addComponents() {
-        if (this.authorization) {
+        if (this.authorization === false) {
             ReactDOM.render(this.getSmartTubeContainer(), document.getElementById('root'));
 
         } else {
-            ReactDOM.render(this.getAuthorizationContainer(), document.getElementById('root'));
+            console.log('tut')
+            ReactDOM.render(<GoogleLogin
+                clientId="549983921860-7ctt7f0r7777vuir9e1pe3bj7p1siea6.apps.googleusercontent.com"
+                buttonText="Login"
+                scope="https://www.googleapis.com/auth/youtube"
+                onSuccess={this.responseGoogle.bind(this)}
+                onFailure={this.responseGoogle}
+            />, document.getElementById('root'));
         }
 
 
-
-    }
-
-    getAuthorizationContainer() {
 
     }
 
