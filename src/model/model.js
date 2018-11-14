6ab96ba6f758fe2bcc03707f18dc3d1ba1a9a26a
wluka85 <wluka85@gmail.com>
2018-11-14 22:20:36 +0100

    Add observer object to model

--- src/model/model.js
+++ src/model/model.js
@@ -1,5 +1,21 @@
 export default class Model {
-    constructor() {
 
+    constructor() {
+        this.observerList = [];
+    }
+
+    attach(observer) {
+        this.observerList.push(observer);
+    }
+
+    detach(observer) {
+        let index = this.observerList.indexOf(observer);
+        this.observerList.splice(index, 1);
+    }
+
+    notifyAllObservers() {
+        this.observerList.forEach(element => {
+            element.update(this);
+        });
     }
 }
