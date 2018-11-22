df59a7ba94366aa15c6fa6202cfb5a9642001f53
wluka85 <wluka85@gmail.com>
2018-11-23 00:04:09 +0100

    Change style authorization view

--- src/view/components/authorizationComponent.js
+++ src/view/components/authorizationComponent.js
@@ -1,6 +1,6 @@
 import GoogleLogin from "react-google-login";
 import React, { Component } from 'react'
-// import '../../css/signIn.css';
+import '../../css/signIn.css';
 
 export default class AuthorizationComponent extends Component {
     constructor(props) {
@@ -15,15 +15,17 @@ export default class AuthorizationComponent extends Component {
 
     render() {
         return(
-            <div id="sign-in-container">
-                <GoogleLogin id='sign-in-button'
-                    clientId="549983921860-7ctt7f0r7777vuir9e1pe3bj7p1siea6.apps.googleusercontent.com"
-                    buttonText="Sign In"
-                    scope="https://www.googleapis.com/auth/youtube"
-                    onSuccess={this.handleSingIn.bind(this)}
-                    onFailure={this.handleOnFailure.bind(this)}
-                />
-                <div id='sign-in-message'>{ this.state.message }</div>
+            <div className="sign-in-window">
+                <div id="sign-in-container">
+                    <GoogleLogin id='sign-in-button'
+                        clientId="549983921860-7ctt7f0r7777vuir9e1pe3bj7p1siea6.apps.googleusercontent.com"
+                        buttonText="Sign In"
+                        scope="https://www.googleapis.com/auth/youtube"
+                        onSuccess={this.handleSingIn.bind(this)}
+                        onFailure={this.handleOnFailure.bind(this)}
+                    />
+                    <div id='sign-in-message'>{ this.state.message }</div>
+                </div>
             </div>
         )
     }
