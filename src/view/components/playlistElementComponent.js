920549994aceebcb50d2eeaa1982f357a2276209
wluka85 <wluka85@gmail.com>
2018-11-25 01:19:53 +0100

    Move onclick play action from image to button play

--- src/view/components/playlistElementComponent.js
+++ src/view/components/playlistElementComponent.js
@@ -6,11 +6,11 @@ export default class PlaylistElementComponent extends Component {
     render() {
         return (
             <div className="playlist-element-component">
-                <img src={this.props.imageURL} alt={this.props.title} className="video-picture" onClick={() => {this.props.playlistElementAction(this.props.video)}}/>
+                <img src={this.props.imageURL} alt={this.props.title} className="video-picture"/>
                 <div className="video-name"> {this.props.name}</div>
                 <div className="video-buttons">
                     <i className="fas fa-trash-alt" onClick={() => {this.props.deleteAction(this.props.playlistElementId)}}/>
-                    <i className="fas fa-play" id="play-button"/>
+                    <i className="fas fa-play" id="play-button" onClick={() => {this.props.playlistElementAction(this.props.video)}}/>
                 </div>
             </div>
         )
