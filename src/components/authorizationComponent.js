cf0b5b1c5ac1eff34ec971894e974289b14bf56a
wluka85 <wluka85@gmail.com>
2018-11-27 20:26:55 +0100

    Add logout button with action onClick and refactor code

--- src/components/authorizationComponent.js
+++ src/components/authorizationComponent.js
@@ -2,23 +2,31 @@ import GoogleLogin from "react-google-login";
 import React, { Component } from 'react'
 // import '../../css/signIn.css';
 import {connect} from 'react-redux';
+import LogoutButton from "../view/components/logoutButton";
 
 class AuthorizationComponent extends Component {
 
     render() {
-        const { handleUserLogin, handleLoginFailure, message } = this.props;
+        const { handleUserLogin, handleLoginFailure, handleUserLogout, message, accessToken } = this.props;
+        let googleButton;
+        if (accessToken.length === 0) {
+            googleButton = <GoogleLogin id='sign-in-button'
+                                        clientId="549983921860-7ctt7f0r7777vuir9e1pe3bj7p1siea6.apps.googleusercontent.com"
+                                        buttonText="Sign In"
+                                        scope="https://www.googleapis.com/auth/youtube"
+                                        onSuccess={handleUserLogin}
+                                        onFailure={handleLoginFailure}
+            />;
+
+        } else {
+            googleButton = <LogoutButton actionOnLogout={handleUserLogout}/>;
+        }
 
         return(
             <div className="sign-in-window">
                 <div id="sign-in-container">
-                    <GoogleLogin id='sign-in-button'
-                        clientId="549983921860-7ctt7f0r7777vuir9e1pe3bj7p1siea6.apps.googleusercontent.com"
-                        buttonText="Sign In"
-                        scope="https://www.googleapis.com/auth/youtube"
-                        onSuccess={handleUserLogin}
-                        onFailure={handleLoginFailure}
-                    />
-                    <div id='sign-in-message'>Message: { message }</div>
+                    {googleButton}
+                    <div id='sign-in-message'>{ message }</div>
                 </div>
             </div>
         )
@@ -26,7 +34,7 @@ class AuthorizationComponent extends Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log('MSTP ', state.authReducer.accessToken);
+
     return {
         accessToken: state.authReducer.accessToken,
         message: state.authReducer.message
@@ -36,8 +44,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 
     return {
-        handleUserLogin: (response) => dispatch({type: 'USER_LOGGED_IN', accessToken: response.accessToken}),
-        handleLoginFailure: () => dispatch({type: 'USER_NOT_LOGGED_IN', message: 'Ops! Wrong Login or Password'})
+        handleUserLogin: (response) => dispatch({type: 'USER_LOGGED_IN', message: '', accessToken: response.accessToken}),
+        handleLoginFailure: () => dispatch({type: 'USER_NOT_LOGGED_IN', message: 'Ops! Wrong Login or Password'}),
+        handleUserLogout: () => dispatch({type: 'USER_LOGGED_OUT', accessToken: '', message: ''})
     };
 };
 
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -6,16 +6,18 @@ const initialState = {
 const authReducer = (state=initialState, action) => {
   switch(action.type) {
     case 'USER_LOGGED_IN':
-    console.log('were in reducer', action.accessToken);
-      return {...state, message: 'Cool!', accessToken: action.accessToken};
+
+      return {...state, message: action.message, accessToken: action.accessToken};
     case 'USER_NOT_LOGGED_IN':
-    console.log('were not in reducer', action.message);
     return {...state, message: action.message};
+
+      case 'USER_LOGGED_OUT':
+        return {...state, message: action.message, accessToken: action.accessToken};
+
     default:
       return state;  
   }
   
 };
-
 
 export default authReducer;
