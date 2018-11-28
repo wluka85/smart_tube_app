116bf2cec36208b38da4d1620a5f8be9afdbcddd
Senseinvader <d.ivantsov1983@gmail.com>
2018-11-28 21:05:03 +0100

    Create videoActions and videoReducer to operate video playback

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 import searchReducer from './searchReducer';
+import videoReducer from './videoReducer';
 
 
 const rootReducer = combineReducers({
   authReducer,
-  searchReducer
+  searchReducer,
+  videoReducer
 });
 
 
