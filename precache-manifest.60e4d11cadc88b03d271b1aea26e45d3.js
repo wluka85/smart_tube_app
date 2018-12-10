10cb0b246f40f8a23269c1b76f2416e264248e41
Senseinvader <d.ivantsov1983@gmail.com>
2018-12-10 10:40:17 +0100

    Updates

---
+++ precache-manifest.60e4d11cadc88b03d271b1aea26e45d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ac53ee6eb305c80a468",
+    "url": "/smart_tube_app/static/js/runtime~main.3ac53ee6.js"
+  },
+  {
+    "revision": "eb3fd251c254c80dc561",
+    "url": "/smart_tube_app/static/js/main.eb3fd251.chunk.js"
+  },
+  {
+    "revision": "788688e1de1d89d6ac81",
+    "url": "/smart_tube_app/static/js/1.788688e1.chunk.js"
+  },
+  {
+    "revision": "eb3fd251c254c80dc561",
+    "url": "/smart_tube_app/static/css/main.2aedaab1.chunk.css"
+  },
+  {
+    "revision": "306b54c6d3a99127b53f23174758cce6",
+    "url": "/smart_tube_app/index.html"
+  }
+];
