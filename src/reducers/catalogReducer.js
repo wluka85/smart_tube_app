8258b6fa08ab3c64a93d3735301dbae71ea2d7aa
wluka85 <wluka85@gmail.com>
2019-05-14 21:54:31 +0200

    Implement actions for geting user catalogs in catalogReducer module

--- src/reducers/catalogReducer.js
+++ src/reducers/catalogReducer.js
@@ -1,12 +1,20 @@
 const initialState = {
     catalogList: [],
-    showAddNewCatalogWindow: false
+    showAddNewCatalogWindow: false,
+    isFetching: false,
+    errorMessage: '',
 };
 
 const catalogReducer = (state=initialState, action) => {
     switch(action.type) {
-        case 'FETCH_CATALOGS_SUCCESS':
-            return {...state, catalogList: action.catalogList};
+        case 'GET_USER_CATALOGS_REQUESTED':
+            return { ...state, isFetching: true }
+        
+        case 'GET_USER_CATALOGS_SUCCESS':
+            return {...state, catalogList: action.catalogList, isFetching: false };
+
+        case 'GET_USER_CATALOGS_FAILED':
+            return {...state, isFetching: false, errorMessage: action.errorMessage }
 
         case 'SHOW_ADD_NEW_CATALOG':
             return {...state, showAddNewCatalogWindow: action.showAddNewCatalogWindow};
