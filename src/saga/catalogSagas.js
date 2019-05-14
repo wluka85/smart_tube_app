32e1eb0b8c17c0c0db881195b94bed31606b131b
wluka85 <wluka85@gmail.com>
2019-05-15 00:37:32 +0200

    Implement addUserCatalog and addUserCatalogSaga functions in catalogSags module

--- src/saga/catalogSagas.js
+++ src/saga/catalogSagas.js
@@ -1,6 +1,6 @@
-import { urlSearchCatalogs } from "../model/apiQueries";
-import { getCatalogs } from "../model/playlist";
-import { userCatalogsFetched } from '../actions/catalogAction';
+import { urlSearchCatalogs, addUserCatalogUrl } from "../model/apiQueries";
+import Playlist, { getCatalogs } from "../model/playlist";
+import { userCatalogsFetched, addUserCatalogSuccess, addUserCatalogFailed, getUserCatalogsFailed } from '../actions/catalogAction';
 import fetch from 'isomorphic-fetch';
 import { call, put, takeEvery } from 'redux-saga/effects'
 
@@ -8,14 +8,43 @@ export function* getUserCatalogsData(dispatch, getState) {
   try {
     const response = yield call(fetch, urlSearchCatalogs, {
       method: 'GET',
-      headers: new Headers({ 'Authorization': 'Bearer ' + encodeURIComponent(getState().authReducer.accessToken) })
+      headers: new Headers({ 'Authorization': `Bearer ${getState().authReducer.accessToken}` })
     })
     const data = yield response.json();
     let catalogs = getCatalogs(data.items);
     yield put(userCatalogsFetched(catalogs))
   } catch {
-    // TODO action for faild and case in reducer
+    yield put(getUserCatalogsFailed('Error!'));
   }
+}
+
+export function* addUserCatalog(dispatch, getState, action) {
+  try {
+    const response = yield call(fetch, addUserCatalogUrl, {
+      method: 'POST',
+      headers: new Headers({ 'Authorization': 'Bearer ' + encodeURIComponent(getState().authReducer.accessToken),
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'}),
+      body: JSON.stringify({snippet: {
+              "title": action.title,
+              "description": action.description
+          }
+        })
+      })
+  
+    const data = yield response.json();
+    const { id, etag } = data;
+    const { title, description, publishedAt } = data.snippet;
+    const catalog = new Playlist(id, title, description, etag, publishedAt);
+    console.log('catalog', catalog);
+    yield put(addUserCatalogSuccess(catalog));
+  } catch {
+    yield put(addUserCatalogFailed('Error Playlist hasn\'t been added!'));
+  }
+}
+
+export function* addUserCatalogSaga(dispatch, getState) {
+  yield takeEvery('ADD_USER_CATALOG_REQUESTED', (action) => addUserCatalog(dispatch, getState, action));
 }
 
 export function* getUserCatalogsSaga(dispatch, getState) {
