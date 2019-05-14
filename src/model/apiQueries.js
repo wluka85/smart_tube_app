969c7be1684a3d4b53d4207ccc54d2c9ac2cde4f
wluka85 <wluka85@gmail.com>
2019-05-15 00:39:56 +0200

    Add constant of add catalog url in apiQueries module

--- src/model/apiQueries.js
+++ src/model/apiQueries.js
@@ -1,2 +1,3 @@
 const api = 'https://www.googleapis.com/youtube/v3/';
 export const urlSearchCatalogs = `${api}playlists?part=snippet&maxResults=30&mine=true`;
+export const addUserCatalogUrl = `${api}playlists?part=snippet`;
