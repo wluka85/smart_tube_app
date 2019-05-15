80162961aec4803be7171c922d8247e2028c1afe
wluka85 <wluka85@gmail.com>
2019-05-16 00:43:31 +0200

    Add url query for search results action

--- src/model/apiQueries.js
+++ src/model/apiQueries.js
@@ -1,4 +1,6 @@
 const api = 'https://www.googleapis.com/youtube/v3/';
+const API_KEY = 'AIzaSyBYOluBSrsLsqs0xGpRPueAUsOujDYdECc';
 export const urlSearchCatalogs = `${api}playlists?part=snippet&maxResults=30&mine=true`;
 export const addUserCatalogUrl = `${api}playlists?part=snippet`;
 export const getDeleteUserCatalogUrl = (playlistId) => `${api}playlists?id=${playlistId}`;
+export const getSearchResultsUrl = (searchCriteria) => `${api}search?part=snippet&q=${searchCriteria}&maxResults=30&type=video&key=${API_KEY}&safeSearch=strict`;
